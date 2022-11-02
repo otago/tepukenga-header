4adf3cd77b7eb424b41222b0e7a60bd655f70ebe
Jeffrey Dick <jeffrey@jeffreydick.com>
2022-11-03 11:49:07 +1300

    Links inside links

--- src/OP/Header/Extensions/Link.php
+++ src/OP/Header/Extensions/Link.php
@@ -3,12 +3,38 @@
 namespace OP\Header\Extensions;
 
 use OP\Header\Model\Header;
+use SilverStripe\Forms\FieldList;
+use SilverStripe\Forms\GridField\GridField;
+use SilverStripe\Forms\GridField\GridFieldConfig_RecordEditor;
 use SilverStripe\ORM\DataExtension;
+use Symbiote\GridFieldExtensions\GridFieldOrderableRows;
+use gorriecoe\Link\Models\Link as ModelsLink;
 
 class Link extends DataExtension
 {
+    private static $db = [
+        'Sort' => 'Int'
+    ];
+
     private static $has_one = [
         'Header' => Header::class,
-        'TopHeader' => Header::class
+        'TopHeader' => Header::class,
+        'Link' => ModelsLink::class
     ];
+
+    private static $has_many = [
+        'Links' => ModelsLink::class
+    ];
+
+    private static $default_sort = 'Sort ASC';
+
+    public function updateCMSFields(FieldList $fields)
+    {
+        $links = GridField::create("Links", "Links", $this->owner->Links())
+            ->setConfig(
+                (new GridFieldConfig_RecordEditor())
+                    ->addComponent(new GridFieldOrderableRows())
+            );
+        $fields->addFieldToTab("Root.Links", $links);
+    }
 }
--- src/OP/Header/Model/Header.php
+++ src/OP/Header/Model/Header.php
@@ -4,6 +4,7 @@ namespace OP\Header\Model;
 
 use SilverStripe\ORM\DataObject;
 use gorriecoe\Link\Models\Link;
+use Symbiote\GridFieldExtensions\GridFieldOrderableRows;
 
 class Header extends DataObject
 {
@@ -12,7 +13,22 @@ class Header extends DataObject
     ];
 
     private static $has_many = [
-        'Items' => Link::class . ".Header",
-        'TopItems' => Link::class . ".TopHeader"
+        'Links' => Link::class . ".Header",
+        'TopLinks' => Link::class . ".TopHeader"
     ];
+
+    public function getCMSFields()
+    {
+        $fields = parent::getCMSFields();
+
+        if ($links = $fields->fieldByName("Root.Links.Links")) {
+            $links->getConfig()->addComponent(new GridFieldOrderableRows());
+        }
+
+        if ($top_links = $fields->fieldByName("Root.TopLinks.TopLinks")) {
+            $top_links->getConfig()->addComponent(new GridFieldOrderableRows());
+        }
+
+        return $fields;
+    }
 }
