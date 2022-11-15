29c610e1df1b5398752d726c9e7683d05029572c
Jeffrey Dick <jeffrey@jeffreydick.com>
2022-11-15 13:11:18 +1300

    Cleanup

--- client/src/boot/index.js
+++
@@ -1,8 +0,0 @@
-/* global window */
-// import registerComponents from 'boot/registerComponents';
-import ToggleNavigation from '../components/ToggleNavigation';
-
-window.document.addEventListener('DOMContentLoaded', () => {
-  ToggleNavigation();
-  // registerComponents();
-});
